2bdfd794f9b19b4071fd4b7f0477a7f4bb872dd1
Rutuja9696 <rutuja.patil@contentstact.com>
2021-03-15 17:12:56 +0530

    added header compnent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import store from "./app/modules/redux/store/store";
 import { Provider } from "react-redux";
 import Contacts from "../src/app/commons/components/AddContact";
+import Header from "../src/app/commons/components/Header";
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <Header />
         <Contacts></Contacts>
       </div>
     </Provider>
