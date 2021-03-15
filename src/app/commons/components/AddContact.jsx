e08b1dcf08075a7ac0a1a2a4f6498d61bdc589b5
Rutuja9696 <rutuja.patil@contentstact.com>
2021-03-15 12:29:11 +0530

    added function to delete contact

--- src/app/commons/components/AddContact.jsx
+++ src/app/commons/components/AddContact.jsx
@@ -1,16 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { connect } from "react-redux";
 import store from "../../modules/redux/store/store";
 import { contactActionTypes } from "../../modules/redux/constants/contactActionTypes";
 import contactActionObjectGenerator from "../../modules/redux/action/contactAction";
 import NewContact from "./Form";
+import url from "../services/api/fetchData";
 
 function Contacts(props) {
   //using  hook
   useEffect(() => {
     store.dispatch(contactActionObjectGenerator(contactActionTypes.GET));
   }, []);
-
+  //delete contact
+  const deleteContact = (event) => {
+    let id = event.target.parentNode.parentNode.id;
+    fetch(url + id, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        alert("Deleted contact successfully !");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   console.log(props);
   //markup
   return (
@@ -27,16 +47,25 @@ function Contacts(props) {
                 <th>Email Id</th>
                 <th>Phone no</th>
                 <th>Status</th>
+                <th>Actions</th>
               </tr>
               {props.contactProps.data.map((contact) => {
                 return (
                   <>
-                    <tr>
+                    <tr key={contact.contactId} id={contact.contactId}>
                       <td>{contact.firstName}</td>
                       <td>{contact.lastName}</td>
                       <td>{contact.email}</td>
                       <td>{contact.phone}</td>
                       <td>{contact.status}</td>
+                      <td>
+                        <input
+                          type="button"
+                          value="X"
+                          onClick={deleteContact}
+                        />
+                        <button>Edit</button>
+                      </td>
                     </tr>
                   </>
                 );
